a2b6a787a547c98e3e78654476276ff0cb2c82cd
daarani24 <daarani2007@gmail.com>
2026-02-26 23:08:26 +0530

    Added reverse program

---
+++ Arrays/reverse.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+public class reverse{
+    public static void main(String ara[]){
+        Scanner s=new Scanner(System.in);
+        int n=s.nextInt();
+        int a[]=new int[n];
+        for(int i=0;i<n;i++)
+            a[i]=s.nextInt();
+        int i=0,j=n-1;
+        while(i<j){
+            int t=a[i];
+            a[i]=a[j];
+            a[j]=t;
+            i++;
+            j--;
+        }
+        for(int k=0;k<n;k++){
+            System.out.print(a[k]+" ");
+        }
+    }
+}
