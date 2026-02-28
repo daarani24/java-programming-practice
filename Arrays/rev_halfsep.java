d72e803eaabac27edabeeb0b6b60f2fc864f6bd2
daarani24 <daarani2007@gmail.com>
2026-02-28 22:16:00 +0530

    practiced reversing each half separatel in java program

---
+++ Arrays/rev_halfsep.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+public class rev_halfsep {
+    public static void main(String ara[]){
+        Scanner s=new Scanner(System.in);
+        int n=s.nextInt();
+        int a[]=new int[n];
+        for (int i=0;i<n;i++){
+            a[i]=s.nextInt();
+        }
+        rev(a);
+    }
+    static void rev(int a[]){
+        int n=a.length;
+        int i=0,j=(n/2)-1;
+        while (i<j){
+            int t=a[i];
+            a[i]=a[j];
+            a[j]=t;
+            i++;
+            j--;
+        }
+        i=(n/2);
+        j=n-1;
+        while(i<j){
+            int t=a[i];
+            a[i]=a[j];
+            a[j]=t;
+            i++;
+            j--;
+        }
+        for (i=0;i<n;i++)
+            System.out.print(a[i]+" ");
+    }
+}
+
